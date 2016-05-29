5c647ff8eabd312b4fa28ef7ca7e262d75e8b40e
gtostain <tostain.guillaume1@gmail.com>
2016-05-29 17:47:29 +0200

    add admin crud

---
+++ src/Ens/TostainGuillaumeBundle/Controller/CategoryAdminController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gtostain
+ * Date: 29/05/2016
+ * Time: 17:05
+ */
+
+namespace Ens\TostainGuillaumeBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class CategoryAdminController extends Controller
+{
+
+}
